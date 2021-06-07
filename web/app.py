5f8804e79cbaf75366054f6089c92ad7895fbbee
Philipp Freilinger <philipp@freilinger.org>
2021-06-07 14:38:37 +0200

    console output function

--- web/app.py
+++ web/app.py
@@ -1,14 +1,23 @@
 #Import the flask module
 from flask import Flask
+import sys
 
 #Create a Flask constructor. It takes name of the current module as the argument
 app = Flask(__name__)
 
 #Create a route decorator to tell the application, which URL should be called for the #described function and define the function
+def ConsoleLog(text):
+    print(text, file=sys.stderr)
 
 @app.route('/')
 def home():
-    return " D’oh!dfgsdgdg"
+    return " D’oh!"
+
+
+@app.route('/test')
+def test():
+    ConsoleLog('Hello world!')
+    return "nix"
 
 #Create the main driver function
 if __name__ == '__main__':
